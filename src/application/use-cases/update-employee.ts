42274d7b082517c0aa436e08d5cc77ffd4e74b98
Conrado Medeiros <conradodemedeiros@hotmail.com>
2024-06-30 16:18:12 -0300

    feat(application): implement update employee use case

---
+++ src/application/use-cases/update-employee.ts
@@ -0,0 +1,24 @@
+import { IEmployeeRepository } from "../interfaces/IEmployeeRepository";
+import { Employee } from "../../domain/entities/employee";
+import { EmployeeId } from "../../domain/value-objects/employee-id";
+import { EmployeeName } from "../../domain/value-objects/employee-name";
+import { EmployeeAge } from "../../domain/value-objects/employee-age";
+
+export class UpdateEmployee {
+  constructor(private employeeRepository: IEmployeeRepository) {}
+
+  async execute(
+    id: string,
+    name: string,
+    age: number,
+    position: string
+  ): Promise<void> {
+    const employee = new Employee(
+      new EmployeeId(id),
+      new EmployeeName(name),
+      new EmployeeAge(age),
+      position
+    );
+    await this.employeeRepository.update(employee);
+  }
+}
