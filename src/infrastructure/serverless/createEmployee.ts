2f763e2a8163544de62eea764f0296b93bd8cb44
Conrado Medeiros <conradodemedeiros@hotmail.com>
2024-06-30 17:03:58 -0300

    feat(infrastructure): implement createEmployee lambda function

---
+++ src/infrastructure/serverless/createEmployee.ts
@@ -0,0 +1,25 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { CreateEmployee } from "../../application/use-cases/create-employee";
+import { EmployeeRepository } from "../database/employee-repository";
+
+// Inicializa o repositório de funcionários
+const employeeRepository = new EmployeeRepository();
+
+export const createEmployee: APIGatewayProxyHandler = async (event) => {
+    try {
+        const { id, name, age, position } = JSON.parse(event.body || '{}');
+
+        const createEmployee = new CreateEmployee(employeeRepository);
+        await createEmployee.execute(id, name, age, position);
+
+        return {
+            statusCode: 201,
+            body: JSON.stringify({ message: 'Employee created successfully' }),
+        };
+    } catch (error) {
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: (error as Error).message }),
+        };
+    }
+};
