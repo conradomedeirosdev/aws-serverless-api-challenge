8d16091acb463b8fc66157609c8a1a4cc448cae8
Conrado Medeiros <conradodemedeiros@hotmail.com>
2024-07-01 15:39:29 -0300

    fix(config): fiz config package, jest and gitignore

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-    testMatch: ['**/*.test.ts']
-  };
-  
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  testTimeout: 10000,
+};
